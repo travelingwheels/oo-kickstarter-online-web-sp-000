5391450fbbdbe2974d8590577ab8d36aba31e342
Gary Wheeler <gkwheeler40@gmail.com>
2019-06-07 15:09:04 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,10 +1,14 @@
 class Project
   
-  attr_accessor :backers
+  attr_accessor :backers, :backer
   attr_reader :title 
   
   def initialize(title)
     @title = title
     @backers = []
+  end
+  
+  def add_backer(backer)
+    @backers << backer 
   end
 end
